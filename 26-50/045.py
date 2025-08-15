cc39f8304b8d4d1a9bb2ecc1f299632f95cd81f5
FraLiturri <francesco.liturri.dev@gmail.com>
2025-08-15 11:05:46 +0200

    Added 046.py

--- 26-50/045.py
+++ 26-50/045.py
@@ -1,8 +1,4 @@
-import numpy as np # Status: work in progress
-
-triangular = np.array([], dtype=int)
-pentagonal = np.array([], dtype=int)
-exagonal = np.array([], dtype=int)
+import numpy as np  # Status: done ✅
 
 
 def triangle_gen(n):
@@ -20,29 +16,22 @@ def exagon_gen(n):
     return res
 
 
-limit = 100
-step = 10
-initial_value = 50
 found = False
-extended = False
-
+t = 286
+p = 165
+e = 143
 while not found:
-    for n in range(initial_value, limit):
-        triangular = np.append(triangular, triangle_gen(n))
-        pentagonal = np.append(pentagonal, pentagonal_gen(n))
-        exagonal = np.append(exagonal, exagon_gen(n))
+    triangular = triangle_gen(t)
+    t += 1
+    while pentagonal_gen(p) <= triangular:
+        p += 1
+        if pentagonal_gen(p) == triangle_gen(t):
+            while exagon_gen(e) < pentagonal_gen(p):
+                e += 1
+                if exagon_gen(e) == pentagonal_gen(p):
+                    found = True
+    if found:
+        break
 
-    diff1 = triangular - pentagonal
-    diff2 = pentagonal - exagonal
 
-    if 0.0 in diff1 and 0.0 in diff2:
-        found = True
-        print(n)
-
-    if not found and not extended:
-        n = n + step
-        limit = limit + step
-        triangular = np.array([])
-        pentagonal = np.array([])
-        exagonal = np.array([])
-        extended = True
+print("The next one is:", triangle_gen(t))
---
+++ 26-50/046.py
@@ -0,0 +1,46 @@
+import numpy as np  # Status: done ✅ (can be improved, it's too orecchietta)
+
+squares = []
+primes = []
+golbach_odds = []
+
+
+def square_gen(n):
+    for i in range(0, n):
+        squares.append(i**2)
+
+
+def prime_gen(n):
+    for j in range(2, n + 1):
+        isPrime = True
+        for i in range(2, int(np.sqrt(j)) + 1):
+            r = j % i
+            if r == 0:
+                isPrime = False
+        if isPrime:
+            primes.append(j)
+    return isPrime
+
+
+prime_gen(10000)
+square_gen(100)
+
+for prime in primes:
+    for square in squares:
+        num = prime + 2 * square
+        golbach_odds.append(num)
+
+golbach_odds.sort()
+golbach_odds = list(dict.fromkeys(golbach_odds))
+
+prev_num = golbach_odds[0]
+for i in range(1, len(golbach_odds)):
+    if (
+        golbach_odds[i] % 2 == 1
+        and prev_num % 2 == 1
+        and golbach_odds[i] - prev_num != 2
+        and golbach_odds[i] < 10**4
+    ):
+        print("found", golbach_odds[i] - 2)
+        break
+    prev_num = golbach_odds[i]
